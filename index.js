a7d80340594d0f99164e2f0dffdccd4570e8b267
m0d1997 <thheodoregarrard@gmail.com>
2022-09-03 14:23:51 -0600

    Completed test, still working on rock paper scissors

--- index.js
+++ index.js
@@ -325,8 +325,27 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+if (score >= 90){
+  console.log("you got an A");
+  return "you got an A";
+}
+else if (score >= 80 && score <= 89) {
+  console.log("you got a B");
+  return "you got a B";
+}
+else if (score >= 70 && score <= 79) {
+  console.log("you got a C");
+  return "you got a C";
+}
+else if (score >= 60 && score <= 69) {
+  console.log("you got a D");
+  return "you got a D";
+}
+else {
+  console.log("you got an F");
+  return "you got an F";
+}
 }
 
 
